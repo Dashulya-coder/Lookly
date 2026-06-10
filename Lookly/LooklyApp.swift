a89b2d575eda7886719d7ef8cb3fdbdc336b1ab5
Dashulya-coder <dasha.ukshe@gmail.com>
2026-06-10 17:14:22 +0300

    Initial commit

---
+++ Lookly/LooklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LooklyApp.swift
+//  Lookly
+//
+//  Created by Daria Ukshe on 06.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct LooklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
